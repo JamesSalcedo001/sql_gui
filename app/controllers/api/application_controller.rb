ccb1b3b8557791d864da6382c8ac0ac0738242eb
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-04-26 20:26:58 -0500

    React and Vite setup

---
+++ app/controllers/api/application_controller.rb
@@ -0,0 +1,10 @@
+module Api
+    class ApplicationController < ActionController::API
+        include ActionController::Cookies
+    
+        def hello_world
+        session[:count] = (session[:count] || 0) + 1
+        render json: { count: session[:count] }
+        end
+    end
+end
