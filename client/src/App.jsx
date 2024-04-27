8ff06d574180b0b12cc036a1e33104c750e1f6ae
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-04-26 20:55:58 -0500

    set up routing in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   useEffect(() => {
     fetch("/api/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+    .then((res) => res.json())
+    .then((data) => setCount(data.count))
+  }, [])
 
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/count" element={<h1>Count: {count} </h1>}/>
+          <Route path="/" element={<h1>Home</h1>} />
+        </Routes>
+      </div>
   )
 }
 
