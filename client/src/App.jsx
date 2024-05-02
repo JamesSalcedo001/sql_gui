c59a96845d82656e28d5f0b5afc086deba5f55a3
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-05-01 23:12:58 -0500

    Add basic styling and sample data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,28 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 
 
+
+
 function App() {
   const [count, setCount] = useState(0)
+
+  const sampleData = [
+    {
+      "id": 1,
+      "name": "James",
+      "age": 28
+    },
+    {
+      "id": 2,
+      "name": "Binx",
+      "age": 3
+    },
+    {
+      "id": 3,
+      "name": "Tomo",
+      "age": 4
+    }
+  ]
 
   useEffect(() => {
     fetch("/api/hello")
@@ -17,7 +37,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/count" element={<h1>Count: {count} </h1>}/>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home sampleData={sampleData} />} />
         </Routes>
       </div>
   )
